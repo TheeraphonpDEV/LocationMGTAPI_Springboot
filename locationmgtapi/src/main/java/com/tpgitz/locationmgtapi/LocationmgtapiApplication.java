942e6d767234b1e2870ad7e10798c3132ec14f17
TPGitz <103758766+TheeraphonpDEV@users.noreply.github.com>
2022-11-10 20:34:27 +0700

    phase4
    implement swagger-ui api

--- locationmgtapi/src/main/java/com/tpgitz/locationmgtapi/LocationmgtapiApplication.java
+++ locationmgtapi/src/main/java/com/tpgitz/locationmgtapi/LocationmgtapiApplication.java
@@ -2,15 +2,11 @@ package com.tpgitz.locationmgtapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class LocationmgtapiApplication {
-
-    @RequestMapping("/")
-    String index() {
-        return "Spring Boot : dont ran out the faith";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(LocationmgtapiApplication.class, args);
